8e3ce9015cde1d62161dc682b69a85436d334a41
Asus <Asus@LAPTOP-2KDOGGR9>
2021-10-30 14:31:38 +0700

    Model PesananFood

---
+++ Tubes_PBO/src/Model/PesananFood.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+import java.util.ArrayList;
+/**
+ *
+ * @author Asus
+ */
+public class PesananFood {
+    private int ID_PesananFood;
+    private String namarestoran;
+    private ArrayList<DetailPesanan> detailpesanan;
+    private int TotalHargaFood;
+
+    public PesananFood(int ID_PesananFood, String namarestoran, ArrayList<DetailPesanan> detailpesanan, int TotalHargaFood) {
+        this.ID_PesananFood = ID_PesananFood;
+        this.namarestoran = namarestoran;
+        this.detailpesanan = detailpesanan;
+        this.TotalHargaFood = TotalHargaFood;
+    }
+
+    public int getID_PesananFood() {
+        return ID_PesananFood;
+    }
+
+    public String getNamarestoran() {
+        return namarestoran;
+    }
+
+    public ArrayList<DetailPesanan> getDetailpesanan() {
+        return detailpesanan;
+    }
+
+    public int getTotalHargaFood() {
+        return TotalHargaFood;
+    }
+
+    public void setID_PesananFood(int ID_PesananFood) {
+        this.ID_PesananFood = ID_PesananFood;
+    }
+
+    public void setNamarestoran(String namarestoran) {
+        this.namarestoran = namarestoran;
+    }
+
+    public void setDetailpesanan(ArrayList<DetailPesanan> detailpesanan) {
+        this.detailpesanan = detailpesanan;
+    }
+
+    public void setTotalHargaFood(int TotalHargaFood) {
+        this.TotalHargaFood = TotalHargaFood;
+    }
+    
+}
