76d9405ec6a2460855eb19c5463933d2f40c5a2a
Dominikus Yudistira Prima Pradana <74886532+DominikusYudistira99@users.noreply.github.com>
2021-10-30 17:55:57 +0700

    Fitur Toupup yang dibuat  oleh Dominikus Yudistira

---
+++ src/yudistira/Toupup.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package yudistira;
+
+/**
+ *
+ * @author yudis
+ */
+public class Toupup {
+ private String metodepembayaran;
+    
+    public Toupup (String Metodepembayaran){
+        this.metodepembayaran = Metodepembayaran;
+       
+    }
+
+    public String getMetodepembayaran() {
+        return metodepembayaran;
+    }
+
+    public void setMetodepembayaran(String metodepembayaran) {
+        this.metodepembayaran = metodepembayaran;
+    }
+    
+}
