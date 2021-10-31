ee10210b5c063b38f6ef05e2fab9a86b9e367050
William <William@William>
2021-10-31 20:33:06 +0700

    Controler

--- Tubes_PBO/src/Controller/Controller.java
+++ Tubes_PBO/src/Controller/Controller.java
@@ -5,12 +5,38 @@
  */
 package Controller;
 
+import Model.User;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  *
  * @author Asus
  */
-public class Controller {
 
-   
+public class Controller {
+    static DatabaseHandler conn = new DatabaseHandler();
     
-}
+    public static ArrayList<User> getAllUsers() {
+        ArrayList<User> users = new ArrayList<>();
+        conn.connect();
+        String query = "SELECT * FROM User";
+        try {
+            Statement stmt = conn.con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                User User = new User();
+                User.setNama(rs.getString("nama"));
+                User.setAlamat(rs.getString("alamat"));
+                User.setUsername(rs.getString("username"));
+                User.setPassword(rs.getString("password"));
+                users.add(User);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return (users);
+    }
+}  
