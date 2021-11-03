f0b3427ad84c7f7d644d383c5bfc9ddf8942af51
Windows <Windows@DESKTOP-CUNSUNL>
2021-11-03 10:08:43 +0700

    Tambah id User

--- Tubes_PBO/src/Model/User.java
+++ Tubes_PBO/src/Model/User.java
@@ -14,53 +14,65 @@ import java.util.ArrayList;
  * @author Asus
  */
 public class User {
+    private int id_User;
     private String nama;
     private String alamat;
     private String username;
     private String password;
+    
+    public User(){
+    
+    }
 
-    public User(String nama, String alamat, String username, String password) {
+    public User(int id_User, String nama, String alamat, String username, String password) {
+        this.id_User = id_User;
         this.nama = nama;
         this.alamat = alamat;
         this.username = username;
         this.password = password;
     }
 
-    public User() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int getId_User() {
+        return id_User;
     }
-    
+
+    public void setId_User(int id_User) {
+        this.id_User = id_User;
+    }
+
     public String getNama() {
         return nama;
-    }
-
-    public String getAlamat() {
-        return alamat;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
     public void setNama(String nama) {
         this.nama = nama;
     }
 
+    public String getAlamat() {
+        return alamat;
+    }
+
     public void setAlamat(String alamat) {
         this.alamat = alamat;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
+    
     
     public boolean cekRegister(String username, String email){
         ArrayList<User> User = Controller.getAllUsers();
