aa4fa614ed7b52e3400e7f0c637f1c31638c81c5
Asus <Asus@LAPTOP-2KDOGGR9>
2021-11-20 14:53:45 +0700

    Update Model Driver

--- Tubes_PBO/src/Model/Driver.java
+++ Tubes_PBO/src/Model/Driver.java
@@ -16,54 +16,59 @@ public class Driver extends User{
     private int status;
     private ArrayList<Pesanan> pesanan;
 
-    public Driver(String jeniskendaraan, String platnomor, int pendapatan, int status, ArrayList<Pesanan> pesanan, String nama, String alamat, String username, String password) {
-        super(nama, alamat, username, password);
+    public Driver() {
+    }
+
+       public Driver(String jeniskendaraan, String platnomor, int pendapatan, int status, ArrayList<Pesanan> pesanan, int id_User, String nama, String alamat, String username, String password) {
+        super(id_User, nama, alamat, username, password);
         this.jeniskendaraan = jeniskendaraan;
         this.platnomor = platnomor;
         this.pendapatan = pendapatan;
         this.status = status;
         this.pesanan = pesanan;
-    }
+    }   
 
     public String getJeniskendaraan() {
         return jeniskendaraan;
-    }
-
-    public String getPlatnomor() {
-        return platnomor;
-    }
-
-    public int getPendapatan() {
-        return pendapatan;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public ArrayList<Pesanan> getPesanan() {
-        return pesanan;
     }
 
     public void setJeniskendaraan(String jeniskendaraan) {
         this.jeniskendaraan = jeniskendaraan;
     }
 
+    public String getPlatnomor() {
+        return platnomor;
+    }
+
     public void setPlatnomor(String platnomor) {
         this.platnomor = platnomor;
+    }
+
+    public int getPendapatan() {
+        return pendapatan;
     }
 
     public void setPendapatan(int pendapatan) {
         this.pendapatan = pendapatan;
     }
 
+    public int getStatus() {
+        return status;
+    }
+
     public void setStatus(int status) {
         this.status = status;
+    }
+
+    public ArrayList<Pesanan> getPesanan() {
+        return pesanan;
     }
 
     public void setPesanan(ArrayList<Pesanan> pesanan) {
         this.pesanan = pesanan;
     }
+
+    
     
     
 }
