fb60862878499a937b24ad419b79debadf4673f8
Windows <Windows@DESKTOP-CUNSUNL>
2021-11-21 16:19:20 +0700

    Bagian Menu Pembayaran Gojek

---
+++ Tubes_PBO/src/View/MenuGojek/PembayaranGojek.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View.MenuGojek;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Sebastian Berto
+ */
+public class PembayaranGojek implements ActionListener{
+    private JFrame framepembayaran = new JFrame();
+    public PembayaranGojek(){
+        
+    }
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        
+    }
+    
+}
