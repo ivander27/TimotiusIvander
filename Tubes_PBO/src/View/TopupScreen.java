c6dd7f1df2258b806a0378839725fa79d9de7a26
Dominikus Yudistira Prima Pradana <74886532+DominikusYudistira99@users.noreply.github.com>
2021-11-23 22:04:17 +0700

    Update TopUpSceren oleh Yudis

--- Tubes_PBO/src/View/TopupScreen.java
+++ Tubes_PBO/src/View/TopupScreen.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package View;
+package yudistira;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
+import java.awt.event.ComponentEvent; // java 17.0.1.0
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,44 +16,26 @@ import javax.swing.JLabel;
  *
  * @author yudistira
  */
-public class TopupScreen {
+ import java.awt.FlowLayout;  
+import javax.swing.JButton;  
+import javax.swing.JFrame;  
+import javax.swing.JLabel;  
+import javax.swing.JPanel;  
+public class TopupScreen {  
+    public static void main(String s[]) {  
+        JFrame frame = new JFrame("Topup Saldo ");  
+        JPanel panel = new JPanel();  
+        panel.setLayout(new FlowLayout());  
+        JLabel label = new JLabel(" Topup Saldo");  
+        JButton button = new JButton();  
+        button.setText("Silahkan klik ");  
+        panel.add(label);  
+        panel.add(button);  
+        frame.add(panel);  
+        frame.setSize(200, 300);  
+        frame.setLocationRelativeTo(null);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        frame.setVisible(true);  
+    }  
+}  
     
-    public TopupScreen() {
-        JFrame frame = new JFrame();
-        frame.setSize(500, 700);
-        frame.setLocationRelativeTo(null);
-
-        //Component: Button
-        JButton button, button1, button2, button3, button4;
-        button = new JButton("left");
-        button1 = new JButton("right");
-        button2 = new JButton("top");
-        button3 = new JButton("bottom");
-        button4 = new JButton("center");
-        JButton button5 = new JButton("blabla");
-        
-        JLabel label4 = new JLabel();
-        button4.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println(button1.getText());
-            }
-        });
-                
-        // Border Layout
-        frame.add(button, BorderLayout.WEST);
-        frame.add(button1, BorderLayout.EAST);
-        frame.add(button2, BorderLayout.NORTH);
-        frame.add(button3, BorderLayout.SOUTH);
-        frame.add(button4, BorderLayout.CENTER);
-        
-        frame.setVisible(true);
-        
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-            }
-        });
-    }
-}
