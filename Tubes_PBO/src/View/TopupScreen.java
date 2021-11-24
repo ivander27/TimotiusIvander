21677a2b0821a71d98152e6871f71b2d0879fab2
Dominikus Yudistira Prima Pradana <74886532+DominikusYudistira99@users.noreply.github.com>
2021-11-24 19:25:38 +0700

    Revisi fitur TopupScreen oleh Yudis

--- Tubes_PBO/src/View/TopupScreen.java
+++ Tubes_PBO/src/View/TopupScreen.java
@@ -4,38 +4,75 @@
  */
 package yudistira;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent; // java 17.0.1.0
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
 /**
  *
  * @author yudistira
  */
- import java.awt.FlowLayout;  
-import javax.swing.JButton;  
-import javax.swing.JFrame;  
-import javax.swing.JLabel;  
-import javax.swing.JPanel;  
-public class TopupScreen {  
-    public static void main(String s[]) {  
-        JFrame frame = new JFrame("Topup Saldo ");  
-        JPanel panel = new JPanel();  
-        panel.setLayout(new FlowLayout());  
-        JLabel label = new JLabel(" Topup Saldo");  
-        JButton button = new JButton();  
-        button.setText("Silahkan klik ");  
-        panel.add(label);  
-        panel.add(button);  
-        frame.add(panel);  
-        frame.setSize(200, 300);  
-        frame.setLocationRelativeTo(null);  
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-        frame.setVisible(true);  
-    }  
-}  
-    
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TopupScreen {
+
+    private JFrame mainFrame;
+    private JLabel headerLabel;
+    private JLabel statusLabel;
+    private JPanel controlPanel;
+    private JLabel msglabel;
+
+    public TopupScreen() {
+        prepareGUI();
+    }
+
+    public static void main(String[] args) {
+        TopupScreen swingContainerDemo = new TopupScreen();
+        swingContainerDemo.showJFrameDemo();
+    }
+
+    private void prepareGUI() {
+        mainFrame = new JFrame("Toup Screen");
+        mainFrame.setSize(400, 400);
+        mainFrame.setLayout(new GridLayout(3, 1));
+
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+        headerLabel = new JLabel("", JLabel.CENTER);
+        statusLabel = new JLabel("", JLabel.CENTER);
+        statusLabel.setSize(350, 100);
+        msglabel = new JLabel("Silahkan Pilih Saldo ", JLabel.CENTER);
+       
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout());
+
+        mainFrame.add(headerLabel);
+        mainFrame.add(controlPanel);
+        mainFrame.add(statusLabel);
+        mainFrame.setVisible(true);
+    }
+
+    private void showJFrameDemo() {
+        headerLabel.setText("Silahkan Topup Disini ");
+        final JFrame frame = new JFrame();
+        frame.setSize(300, 300);
+        frame.setLayout(new FlowLayout());
+        frame.add(msglabel);
+
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                frame.dispose();
+            }
+        });
+        JButton okButton = new JButton("Klik");
+        okButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                statusLabel.setText("Halaman Pengguna");
+                frame.setVisible(true);
+            }
+        });
+        controlPanel.add(okButton);
+        mainFrame.setVisible(true);
+    }
+}
